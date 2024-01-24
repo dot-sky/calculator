410f0197ed3fedf739c84c138a60af62ce4db96c
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-01-24 14:53:10 -0400

    Add feature that allows multiple operations

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Calculator(){
         "/": (a,b) => a / b,
     }
     this.calculate = function(a, op, b){
-        return this.operation[op](parseInt(a),parseInt(b));
+        return this.operation[op](parseFloat(a),parseFloat(b));
     };
     this.operate = function(str){
         const op = str.split(" ");
@@ -24,17 +24,23 @@ function addButtons(){
             button.textContent = btnMatrix[i][j];
             button.addEventListener("click", () => {
                 const btnAction = button.getAttribute("id");
-                if (btnAction === "="){
-                    console.log(display.textContent);
-                    display.textContent = calc.operate(display.textContent);
+                if (btnAction === "del" || btnAction === "clear"){
+                    return;
                 }
-                else if (btnAction !== "del" && btnAction !== "clear"){
-                    if (isNaN(btnAction)){
-                        display.textContent += " " + btnAction + " ";
-                    }
-                    else{
-                        display.textContent += btnAction;
-                    }
+                if (!isNaN(btnAction)){
+                    display.textContent += btnAction;
+                }
+                else if(btnAction === "="){
+                    display.textContent = calc.operate(display.textContent);
+                    opCounter = 0;
+                }
+                else if(opCounter > 0){
+                    display.textContent = calc.operate(display.textContent) + " " + btnAction + " ";
+                    opCounter = 1;
+                }
+                else{
+                    display.textContent += " " + btnAction + " ";
+                    opCounter++;
                 }
             });
             row.appendChild(button);
@@ -50,15 +56,41 @@ const btnMatrix = [
     ["x", "0", "x", "+" ]
 ]
 const calc = new Calculator();
+let opCounter = 0;
 const buttonsContainer = document.querySelector(".buttons-cont");
 const display = document.querySelector(".display");
 addButtons();
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () =>{
     display.textContent = "";
+    opCounter = 0;
 })
 const delButton = document.querySelector("#del");
 delButton.addEventListener("click", () =>{
-    display.textContent = display.textContent.slice(0, -1);
+    if (display.textContent.at(-1)===" ")
+        display.textContent = display.textContent.slice(0, -3); 
+    else
+        display.textContent = display.textContent.slice(0, -1); 
 })
 
+ 
+// if (btnAction !== "del" && btnAction !== "clear"){
+//     if (isNaN(btnAction)){
+//         display.textContent += " " + btnAction + " ";
+//         opCounter++;
+//         console.log("enters")
+//     }
+//     else{
+//         display.textContent += btnAction;
+//     }
+// }
+// if (btnAction === "=" || opCounter > 1){
+//     display.textContent = calc.operate(display.textContent) + " " + btnAction + " ";
+//     console.log(display.textContent);
+//     if (btnAction === "=" ){
+//         opCounter = 0;
+//     }
+//     else{
+//         opCounter = 1;
+//     }
+// }
