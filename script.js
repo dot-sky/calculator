9c7640fee16c2be2a333380e22f77c97e33d3763
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-01-24 11:26:07 -0400

    Add calculator elements and modify style

--- script.js
+++ script.js
@@ -9,4 +9,27 @@ function Calculator(){
         return this.operation[op](parseInt(a),parseInt(b));
     };
 }
+function addButtons(){
+    for (let i = 0; i < 5; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        for (let j = 0; j < 4; j++) {
+            const button = document.createElement("div");
+            button.className = "button";
+            button.setAttribute("id",btnMatrix[i][j]);
+            button.textContent = btnMatrix[i][j];
+            row.appendChild(button);
+        }
+        buttonsContainer.appendChild(row);
+    }
+}
+const btnMatrix = [
+    ["clear", "X", "del", "=" ],
+    ["7", "8", "9", "/" ],
+    ["4", "5", "6", "*" ],
+    ["1", "2", "3", "-" ],
+    ["x", "0", "x", "+" ]
+]
 const calc = new Calculator();
+const buttonsContainer = document.querySelector(".buttons-cont");
+addButtons();
