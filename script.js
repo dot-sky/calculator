7351faf2529b5429717e84ff8874f5290b390f0d
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-01-24 15:17:32 -0400

    Refactor event functions

--- script.js
+++ script.js
@@ -22,75 +22,72 @@ function addButtons(){
             button.className = "button";
             button.setAttribute("id",btnMatrix[i][j]);
             button.textContent = btnMatrix[i][j];
-            button.addEventListener("click", () => {
-                const btnAction = button.getAttribute("id");
-                if (btnAction === "del" || btnAction === "clear"){
-                    return;
-                }
-                if (!isNaN(btnAction)){
-                    display.textContent += btnAction;
-                }
-                else if(btnAction === "="){
-                    display.textContent = calc.operate(display.textContent);
-                    opCounter = 0;
-                }
-                else if(opCounter > 0){
-                    display.textContent = calc.operate(display.textContent) + " " + btnAction + " ";
-                    opCounter = 1;
-                }
-                else{
-                    display.textContent += " " + btnAction + " ";
-                    opCounter++;
-                }
-            });
+            addButtonEvents(button);
             row.appendChild(button);
         }
         buttonsContainer.appendChild(row);
     }
+}
+function doCalculation(){
+    lastOp.textContent = display.textContent+" =";
+    display.textContent = calc.operate(display.textContent);
+}
+function addButtonEvents(button){
+    button.addEventListener("click", () => {
+        const btnAction = button.getAttribute("id");
+        if (btnAction === "clear"){
+            display.textContent = "";
+            opCounter = 0;
+            lastOp.textContent = " "; 
+        }
+        else if (btnAction === "del" ){
+            if (display.textContent.at(-1)===" "){
+                display.textContent = display.textContent.slice(0, -3); 
+            }
+            else{
+                display.textContent = display.textContent.slice(0, -1); 
+            }
+        }
+        else if (!isNaN(btnAction)){
+            display.textContent += btnAction;
+        }
+        else if(btnAction === "="){
+            doCalculation();
+            opCounter = 0;
+        }
+        else if(opCounter > 0){
+            doCalculation();
+            display.textContent += " " + btnAction + " ";
+            opCounter = 1;
+        }
+        else{
+            display.textContent += " " + btnAction + " ";
+            opCounter++;
+        }
+    });
 }
 const btnMatrix = [
     ["clear", "x", "del", "=" ],
     ["7", "8", "9", "/" ],
     ["4", "5", "6", "*" ],
     ["1", "2", "3", "-" ],
-    ["x", "0", "x", "+" ]
+    ["-", "0", ".", "+" ]
 ]
 const calc = new Calculator();
 let opCounter = 0;
 const buttonsContainer = document.querySelector(".buttons-cont");
 const display = document.querySelector(".display");
+const lastOp = document.querySelector(".last-op");
 addButtons();
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () =>{
     display.textContent = "";
     opCounter = 0;
+    lastOp.textContent = " "; 
 })
 const delButton = document.querySelector("#del");
 delButton.addEventListener("click", () =>{
-    if (display.textContent.at(-1)===" ")
-        display.textContent = display.textContent.slice(0, -3); 
-    else
-        display.textContent = display.textContent.slice(0, -1); 
+    
 })
 
  
-// if (btnAction !== "del" && btnAction !== "clear"){
-//     if (isNaN(btnAction)){
-//         display.textContent += " " + btnAction + " ";
-//         opCounter++;
-//         console.log("enters")
-//     }
-//     else{
-//         display.textContent += btnAction;
-//     }
-// }
-// if (btnAction === "=" || opCounter > 1){
-//     display.textContent = calc.operate(display.textContent) + " " + btnAction + " ";
-//     console.log(display.textContent);
-//     if (btnAction === "=" ){
-//         opCounter = 0;
-//     }
-//     else{
-//         opCounter = 1;
-//     }
-// }
